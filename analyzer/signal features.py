1a6c53560f3d2a84965532d96dafcece0e7180b9
PawelManczak <pawel14522@gmail.com>
2024-06-01 13:50:22 +0200

    Właściwości bazujące na interwałach między spikeami

--- analyzer/signal features.py
+++ analyzer/signal features.py
@@ -18,25 +18,80 @@ def countSpikes(data):
     return spike_count, spike_indices
 
 
+"""
+    Calculate the average frequency of spikes in the data.
+    
+    f = Number of spikes / time
+"""
+
+
 def average_frequency_of_spikes(data, f=20000):
     spike_count, _ = countSpikes(data)
     return (spike_count / len(data)) * f
 
 
-# Base directory
-file =  '/Users/pawelmanczak/Downloads/pacjenci/extracted_data.csv'
+"""
 
-# Read the CSV file
+    
+    Rf = Number of intervals between 33ms and 66ms / number of intervals
+    
+"""
+
+
+def calculate_intervals(spike_indices, f=20000):
+    # Calculate time intervals between spikes
+    intervals = np.diff(spike_indices) / f * 1000  # Convert to milliseconds
+    return intervals
+
+
+def frequency_coefficient(intervals):
+    # Count intervals in the range 33 < ti < 66
+    valid_intervals = (intervals > 33) & (intervals < 66)
+    N33_66 = np.sum(valid_intervals)
+    N_total = len(intervals)
+
+    if N_total == 0:
+        return 0
+
+    Rf = N33_66 / N_total
+    return Rf
+
+
+"""
+    Launch rate
+    Rb = Number of intervals shorter 33ms / number of intervals
+"""
+
+
+def launch_rate(intervals):
+    valid_intervals = intervals < 33
+    N0_33 = np.sum(valid_intervals)
+    N_total = len(intervals)
+
+    if N_total == 0:
+        return 0
+
+    Rb = N0_33 / N_total
+    return Rb
+
+
+file = '/Users/pawelmanczak/Downloads/pacjenci/extracted_data.csv'
+
 df = pd.read_csv(file)
-
-# Assuming the first row is needed and the columns represent different data points
 data = df.iloc[0, :].dropna().astype(float)
 
-print(len(data))
-
 spike_count, spike_indices = countSpikes(data)
+intervals = calculate_intervals(spike_indices)
+Rf = frequency_coefficient(intervals)
 
-plt.figure(figsize=(10, 6))
+print("Współczynnik częstosci " + str(Rf))
+print("Srednia cczęstotliowść występowania impulsów nerwowych " + str(average_frequency_of_spikes(data)))
+print("Współczynnik wystrzeliwania " + str(launch_rate(intervals)))
+
+#spike_indices = np.array([0, 2000, 4000, 8000, 13000, 20000])
+#print(calculate_intervals(spike_indices))
+
+"""plt.figure(figsize=(10, 6))
 plt.plot(data, label='Signal')
 plt.axhline(y=3 * np.std(data), color='r', linestyle='--', label='Threshold')
 plt.scatter(spike_indices, data.iloc[spike_indices], color='red', label='Spikes')
@@ -44,6 +99,4 @@ plt.xlabel('Sample Index')
 plt.ylabel('Signal Value')
 plt.title(f'Signal with Detected Spikes (Count: {spike_count})')
 plt.legend()
-plt.show()
-
-print(average_frequency_of_spikes(data))
+plt.show()"""

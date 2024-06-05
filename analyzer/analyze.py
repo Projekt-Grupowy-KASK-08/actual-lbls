cbbf45f8fbbedf0a92d05adba7717b4251c3c3e9
kasiaOsowska <96206203+kasiaOsowska@users.noreply.github.com>
2024-06-05 22:58:19 +0200

    statistics on cleared data

--- analyzer/analyze.py
+++ analyzer/analyze.py
@@ -1,53 +1,61 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from signal_features import countSpikes, average_frequency_of_spikes, calculate_intervals, frequency_coefficient, launch_rate, pause_indicator, pause_ratio, modified_launch_rate, standard_deviation_of_duration,data_without_extreme_spikes, average_absolute_difference, rms
 
 # Base directory
-base_dir = 'C:\\Users\\kasia\\OneDrive\\Pulpit\\pacjenci_cleaned\\'
+base_dir = 'C:\\Users\\kasia\\OneDrive\\Pulpit\\pacjenci\\'
 
 # Read the CSV file
-df = pd.read_csv(base_dir + 'label_cleaned.csv')
+df = pd.read_csv(base_dir + 'label_with_file_path.csv')
+
 
 # Iterate over the rows of the DataFrame
 for index, row in df.iterrows():
     # Open csv file of each operation and read it
     csv = pd.read_csv(row['file_path'])
-    segment_size = 5000
-    # Remove artifacts from the signal
-
-
-    # Define start and end of label
-    if row['start'] < csv['Time'].iloc[0]:
-        start_time = csv['Time'].iloc[0]
-        print("przycięto oznaczenie na początku!!!!!!")
-    else:
-        start_time = row['start']
-    if row['end'] > csv['Time'].iloc[-1]:
-        end_time = csv['Time'].iloc[-1]
-        print("przycięto oznaczenie na końcu!!!!!!")
-    else:
-        end_time = row['end']
-
-    # Remove extreme spikes and silent spots from the signal
-   # ranges, csv, start_time, end_time = data_without_extreme_spikes(csv, start_time, end_time, segment_size=2000)
+    all_csv = pd.read_csv(row['file_path'])
 
     # Define class
     label = row['timeserieslabels']
-    if True:  # label == "['Czesci wewnetrzne galki bladej (2-3 mm przed celem lub do 1 mm za celem)']":
+
+    if label == "['Czesci wewnetrzne galki bladej (2-3 mm przed celem lub do 1 mm za celem)']":
+        segment_size = 10000
+
+        _, csv = data_without_extreme_spikes(csv, segment_size=1000)
+        ranges_clean_data, csv = data_without_extreme_spikes(csv, segment_size=1000)
+
+        # Define start and end of label
+        """
+        if row['start'] < csv['Time'].iloc[0]:
+            start_time = csv['Time'].iloc[0]
+            print("przycięto oznaczenie na początku!!!!!!")
+        else:
+            start_time = row['start']
+        if row['end'] > csv['Time'].iloc[-1]:
+            end_time = csv['Time'].iloc[-1]
+            print("przycięto oznaczenie na końcu!!!!!!")
+        else:
+            end_time = row['end']"""
+
+        start_time = row['start']
+        end_time = row['end']
+
         # Create a new figure and add a subplot
-        fig, axs = plt.subplots(2, 1, sharex=True, figsize=(15, 5))
+        fig, axs = plt.subplots(2, 1, sharex=True, figsize=(14, 7))
 
         # Add title and labels
         axs[0].set_title(label)
         axs[0].set_xlabel('Time')
         axs[0].set_ylabel('Signal')
+        # Shadow clean data
+        for start, end in zip(ranges_clean_data['start_time'], ranges_clean_data['end_time']):
+            axs[0].axvspan(start, end, color='gray', alpha=0.5)
+
+
         # Find the indices corresponding to the start and end times
         start_index = csv['Time'].searchsorted(start_time)
         end_index = csv['Time'].searchsorted(end_time)
-
-        # Ensure indices are within bounds
-        start_index = max(0, min(start_index, len(csv) - 1))
-        end_index = max(0, min(end_index, len(csv) - 1))
 
         color = 'gray'
         if label == "['Skorupa lub prazkowie']":
@@ -57,23 +65,45 @@ for index, row in df.iterrows():
         if label == "['Czesci wewnetrzne galki bladej (2-3 mm przed celem lub do 1 mm za celem)']":
             color = 'yellow'
 
-        # Fill the area between the signal and the x-axis
-        print(f'Label: {label}, Start: {start_time}, End: {end_time}')
-        print(f'Start index: {start_index}, End index: {end_index}')
-        print(f'Start value: {csv["2: preprocessed"].iloc[start_index]}, End value: {csv["2: preprocessed"].iloc[end_index]}')
+        # Fill the area between label witj color
         axs[0].axvspan(csv['Time'].iloc[start_index], csv['Time'].iloc[end_index], facecolor=color, alpha=0.3, label=label)
 
         # Split the signal into segments
         segments = [csv['2: preprocessed'][i:i + segment_size] for i in range(0, len(csv['2: preprocessed']), segment_size)]
+
         all_spikes = []
+        all_frequency = []
+        frequency_coefficients = []
+        launch_rates = []
+        pause_indicators = []
+        pause_ratios = []
+        modified_launch_rates = []
+        standard_deviations = []
+        low_band_powers = []
+        high_band_powers = []
+        nonlinear_energies = []
+        absolute_differences = []
+        rms_values = []
+
         # Process each segment separately
         for i, segment in enumerate(segments):
-            # Define a threshold for this segment
-            threshold = 3 * np.std(segment)
 
             # Find where the signal in this segment exceeds the threshold
-            spikes = np.where(segment > threshold)[0]
+            _, spikes = countSpikes(segment)
             all_spikes.append(spikes)
+
+            f = average_frequency_of_spikes(segment, f=segment_size)
+            all_frequency.append(f)
+
+            intervals = calculate_intervals(spikes)
+            frequency_coefficients.append(frequency_coefficient(intervals))
+            launch_rates.append(launch_rate(intervals))
+            pause_indicators.append(pause_indicator(intervals))
+            pause_ratios.append(pause_ratio(intervals))
+            modified_launch_rates.append(modified_launch_rate(intervals))
+            standard_deviations.append(standard_deviation_of_duration(intervals))
+            absolute_differences.append(average_absolute_difference(segment))
+            rms_values.append(rms(segment))
 
             # Plot the signal for this segment
             axs[0].plot(csv['Time'][i * segment_size:(i + 1) * segment_size], segment, linewidth=0.5)
@@ -82,26 +112,47 @@ for index, row in df.iterrows():
             start_index = i * segment_size
             end_index = min((i + 1) * segment_size, len(csv['Time']))
 
+            left_neighbour_segment = 10
+            right_neighbour_segment = 10
+            if i < left_neighbour_segment:
+                left_neighbour_segment = i
+            elif i + right_neighbour_segment > len(segment):
+                right_neighbour_segment = len(segment) - i
+            threshold = 10 * np.std(
+                csv.iloc[(i - left_neighbour_segment) * segment_size:(i + 1 + right_neighbour_segment) * segment_size][
+                    "2: preprocessed"])
+
             # Plot the threshold
-            axs[0].plot([csv['Time'].iloc[start_index], csv['Time'].iloc[end_index - 1]], [threshold, threshold], color='r', linestyle='--', linewidth=0.5)
+            axs[0].plot([csv['Time'].iloc[start_index], csv['Time'].iloc[end_index - 1]], [threshold, threshold], color='r',
+                        linestyle='--', linewidth=0.5)
 
             # Plot the spikes
-            axs[0].plot(csv['Time'].iloc[i * segment_size + spikes], csv['2: preprocessed'].iloc[i * segment_size + spikes], 'r.', markersize=2)
+            axs[0].plot(csv['Time'].iloc[i * segment_size + spikes], csv['2: preprocessed'].iloc[i * segment_size + spikes],
+                        'r.', markersize=2)
 
-        # Plot the distribution of spikes per segment in the second subplot
         num_segments = len(segments)
-
         # Calculate the midpoint of each segment
-        midpoints = [(csv['Time'].iloc[i * segment_size] + csv['Time'].iloc[min((i + 1) * segment_size, len(csv['Time'])) - 1]) / 2 for i in range(num_segments)]
+        midpoints = [
+            (csv['Time'].iloc[i * segment_size] + csv['Time'].iloc[min((i + 1) * segment_size, len(csv['Time'])) - 1]) / 2
+            for i in range(num_segments)]
 
         # Plot the distribution of spikes per segment in the second subplot
         total_time = csv['Time'].iloc[-1] - csv['Time'].iloc[0]
-        bar_width = total_time / num_segments
-        axs[1].bar(midpoints, [len(spikes) for spikes in all_spikes], width=bar_width)
+
+
+        # Plot the statistics in the fourth subplot
+        axs[1].plot(midpoints, frequency_coefficients, label='Frequency Coefficient', color='purple')
+        axs[1].plot(midpoints, launch_rates, label='Launch Rate', color='orange')
+        axs[1].plot(midpoints, pause_indicators, label='Pause Indicator', color='green')
+        axs[1].plot(midpoints, pause_ratios, label='Pause Ratio', color='red')
+        axs[1].plot(midpoints, modified_launch_rates, label='Modified Launch Rate', color='blue')
+        #axs[1].plot(midpoints, standard_deviations, label='Standard Deviation', color='brown')
+        axs[1].plot(midpoints, absolute_differences, label='Absolute Difference', color='magenta')
+        axs[1].plot(midpoints, rms_values, label='RMS', color='yellow')
         axs[1].set_xlabel('Time')
-        axs[1].set_ylabel('Number of spikes')
-        axs[1].set_title('Number of spikes per segment')
+        axs[1].set_ylabel('Value')
+        axs[1].set_title('Segment Statistics')
+        axs[1].legend()
 
         plt.tight_layout()
-        plt.legend()
         plt.show()

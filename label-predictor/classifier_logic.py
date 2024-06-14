73596b91ce94443418c2b7b1792eb857448cf1ed
kasiaOsowska <96206203+kasiaOsowska@users.noreply.github.com>
2024-06-14 14:50:32 +0200

    fixed classifier

--- label-predictor/classifier_logic.py
+++ label-predictor/classifier_logic.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 
-# Getting only the longest range
 def get_longest_range(ranges):
     max_diff = 0
     start = 0
@@ -14,21 +13,25 @@ def get_longest_range(ranges):
             end = ranges['end_time'][i]
     return start, end
 
-
-# Calculating treashold based on neighbouring segments
+# Calculating threshold based on neighbouring segments
 def calculate_treshold(data, segment, segment_size, i):
     left_neighbour_segment = 10
     right_neighbour_segment = 10
     if i < left_neighbour_segment:
         left_neighbour_segment = i
-    elif i + right_neighbour_segment > len(segment):
-        right_neighbour_segment = len(segment) - i
+    elif i + right_neighbour_segment > len(data) // segment_size:
+        right_neighbour_segment = len(data) // segment_size - i - 1
 
-    threshold = 7 * np.std(
-        data.iloc[(i - left_neighbour_segment) * segment_size:(i + 1 + right_neighbour_segment) * segment_size][
-            "2: preprocessed"])
+    if left_neighbour_segment + right_neighbour_segment <= 1:
+        return np.nan
+
+    segment_data = data.iloc[(i - left_neighbour_segment) * segment_size : (i + 1 + right_neighbour_segment) * segment_size]["2: preprocessed"]
+    if len(segment_data) > 1:
+        threshold = 7 * np.std(segment_data.dropna())
+    else:
+        threshold = np.nan
+
     return threshold
-
 
 def classify(data, segment_size):
     ranges = {
@@ -39,8 +42,8 @@ def classify(data, segment_size):
     segments = int(len(data) / segment_size)
     previous_was_accepted = False
     last_end_time = 0
-    for i in range(segments):
 
+    for i in range(segments):
         segment_start = i * segment_size
         segment_end = (i + 1) * segment_size
         if segment_end > len(data):
@@ -48,6 +51,9 @@ def classify(data, segment_size):
 
         segment = data.iloc[segment_start:segment_end]
         threshold = calculate_treshold(data, segment, segment_size, i)
+        if np.isnan(threshold):
+            continue
+
         extreme_spikes = np.where(segment["2: preprocessed"] > threshold)[0]
         zero_count = np.sum((segment["2: preprocessed"] >= -3) & (segment["2: preprocessed"] <= 3))
 
@@ -61,8 +67,12 @@ def classify(data, segment_size):
                 ranges['end_time'].append(last_end_time)
             previous_was_accepted = False
 
+
     if previous_was_accepted:
         ranges['end_time'].append(last_end_time)
+
+    for i in range(len(ranges['start_time'])):
+        print(f"Start: {ranges['start_time'][i]}, End: {ranges['end_time'][i]}")
 
     start, end = get_longest_range(ranges)
     return start, end
